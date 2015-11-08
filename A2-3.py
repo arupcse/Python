daa0860ac917ae9116f7736afec8f4e67cd8dffe
Arup Kumar Sarker <bd.arup@gmail.com>
2015-11-08 12:25:49 +1100

    Create A2-3.py

---
+++ A2-3.py
@@ -0,0 +1,9 @@
+#Get the total hours
+hrs = raw_input("Enter Hours:")
+hrs = float(hrs)  # convert hours to float
+#Get the rates
+rate = raw_input("Enter Rates:")
+rate = float(rate)
+#Getting the grosspay inside gross pay variable
+pay = hrs * rate
+print pay
